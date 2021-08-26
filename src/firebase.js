85e3a6deedb0c5c918173975424702aa887584c0
john911120 <wtt54@naver.com>
2021-08-26 19:22:19 +0900

    1.0 FirebaseBackup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC98Tj_WHx5zHKkSqfVDsKASg5LgnQ5t1c",
+    authDomain: "nwitter-4c48e.firebaseapp.com",
+    projectId: "nwitter-4c48e",
+    storageBucket: "nwitter-4c48e.appspot.com",
+    messagingSenderId: "1094359740527",
+    appId: "1:1094359740527:web:d7267e852a3186644f9d9a"
+  };
+firebase.initializeApp(firebaseConfig);
