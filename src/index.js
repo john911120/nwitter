a8523411d191b8a15757cdcb4f51d55db1a8111d
john911120 <wtt54@naver.com>
2021-08-26 20:04:49 +0900

    1.0 firebase0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import firebase from "./firebase.js";
+import firebase from "./firebase";
 console.log(firebase);
 
 ReactDOM.render(
